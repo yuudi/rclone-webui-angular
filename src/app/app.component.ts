e198c6e98ee211430209b572c5ecac9e4cec7fb9
yuudi <yuudi@users.noreply.github.com>
2023-07-23 22:59:59 +0000

    :sparkles: dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 import { MatIconRegistry } from '@angular/material/icon';
@@ -10,9 +11,12 @@ import { environment } from 'src/environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @HostBinding('class') className = '';
+
   showRemoteSetting = environment.showRemoteSetting;
   showScheduledJobs = environment.electron;
+  theme: 'light' | 'dark' | 'auto' = 'auto';
   languages = [
     {
       display: 'English',
@@ -28,13 +32,54 @@ export class AppComponent {
     },
   ];
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    private overlay: OverlayContainer,
+  ) {
     iconRegistry.addSvgIcon(
       'github',
       sanitizer.bypassSecurityTrustResourceUrl(
         'assets/icons/github-mark-white.svg',
       ),
     );
+  }
+
+  ngOnInit(): void {
+    const theme = localStorage.getItem('rwa') || 'auto';
+    this.activateThemeSetting(
+      theme as 'light' | 'dark' | 'auto',
+    );
+  }
+
+  activateThemeSetting(theme: 'light' | 'dark' | 'auto') {
+    localStorage.setItem('rwa', theme);
+    if (theme === 'light') {
+      this.activeLightTheme();
+    } else if (theme === 'dark') {
+      this.activeDarkTheme();
+    } else if (theme === 'auto') {
+      if (this.getBrowserPreferDarkTheme()) {
+        this.activeDarkTheme();
+      } else {
+        this.activeLightTheme();
+      }
+    }
+  }
+
+  getBrowserPreferDarkTheme() {
+    const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    return darkQuery.matches;
+  }
+
+  activeDarkTheme() {
+    this.className = 'dark-theme dark-theme-basic';
+    this.overlay.getContainerElement().classList.add('dark-theme');
+  }
+
+  activeLightTheme() {
+    this.className = '';
+    this.overlay.getContainerElement().classList.remove('dark-theme');
   }
 
   activateLanguage(languageCode: string) {
